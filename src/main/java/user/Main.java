c71799c296b10deac9b8c3776a630d28b9b334f4
krook1024 <krook1024@gmail.com>
2020-04-09 22:07:33 +0200

    Added a main class to the user package that utilizes methods declared in UserDao

---
+++ src/main/java/user/Main.java
@@ -0,0 +1,41 @@
+package user;
+
+import user.User;
+import user.UserDao;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:app");
+        jdbi.installPlugin(new SqlObjectPlugin());
+        try (Handle handle = jdbi.open()) {
+            UserDao dao = handle.attach(UserDao.class);
+            dao.createTable();
+
+            User user = User.builder()
+                    .username("johndoe1")
+                    .password("asd")
+                    .name("John Doe")
+                    .email("johndoe@outlook.com")
+                    .gender(User.Gender.MALE)
+                    .dob(LocalDate.parse("1970-01-01"))
+                    .enabled(true)
+                    .build();
+
+            dao.insert(user);
+
+            dao.findByUsername("johndoe1").ifPresent(System.out::println);
+
+            dao.findById(1).ifPresent(System.out::println);
+
+            user = dao.findByUsername("johndoe1").get();
+            dao.delete(user);
+
+            dao.list().stream().forEach(System.out::println); // should print nothing
+        }
+    }
+}
